b738b945757deaf7a4d3fa3aa1822d83e651ed3b
Chong Jin Yi <chong601.13@gmail.com>
2021-02-18 08:36:23 +0000

    Add delete volume from pool with checks (mostly) implemented

---
+++ code_snippets/delete_vol_from_pool.py
@@ -0,0 +1,31 @@
+import libvirt
+from xml.etree import ElementTree
+
+client = libvirt.open("qemu+ssh://chong601@10.102.0.5/system")
+instance_to_delete = 'vm-ubuntu-focal-lxd-1'
+domain = client.lookupByName(instance_to_delete)
+
+# find_disk.py start
+domain = client.lookupByName("vm-ubuntu-focal-lxd-1")
+xml = ElementTree.fromstring(domain.XMLDesc())
+disks = xml.findall("./devices/disk")
+disk_source_path = []
+for disk in disks:
+    if 'file' in disk.find("source").attrib:
+        source = disk.find("source").attrib['file']
+        disk_source_path.append(source)
+    else:
+        source = 'n/a'
+
+# find_disk.py end
+
+pool = client.storagePoolLookupByName('vm-ubuntu-focal-lxd-1')
+volume_libvirt = pool.storageVolLookupByName('disk-2.img')
+print(volume_libvirt.path())
+if domain.state() != libvirt.VIR_DOMAIN_SHUTOFF:
+    print("Error: Instance \"{}\" is still running. Shut down or power off the instance before deleting the volume".format(instance_to_delete))
+elif volume_libvirt.path() in disk_source_path:
+    print("Error: Disk is currently attached to instance \"{}\". Detach the disk first before deleting the volume".format(instance_to_delete))
+else:
+    volume_libvirt.delete()
+    print("Volume deleted successfully.")
