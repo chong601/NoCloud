bbbec8c4fef58f59ee8b13e8974c7a4381ad0281
Chong Jin Yi <chong601.13@gmail.com>
2021-02-19 12:02:34 +0800

    Split out parameters to use from the code

--- code_snippets/attach_disk.py
+++ code_snippets/attach_disk.py
@@ -2,31 +2,42 @@ import libvirt
 from xml.etree import ElementTree
 client = libvirt.open("qemu+ssh://chong601@10.102.0.5/system")
 
+DISK_TYPE= 'file'
+DRIVER_NAME = 'qemu'
+DRIVER_SUBDRIVER = 'qcow2'
+DISK_NAME = 'disk-2.img'
+TARGET_DOMAIN = POOL_NAME = 'vm-ubuntu-focal-lxd-1'
+TARGET_DEVICE = 'sdd'
+TARGET_BUS = 'scsi'
+
+# Source attr contains the details of the source disk, in the
+target_pool = client.storagePoolLookupByName(POOL_NAME)
+target_vol = target_pool.storageVolLookupByName(DISK_NAME)
+
+SOURCE_DISK = target_vol.path()
+
+
 # create XML definition
-disk_attr = {'type': 'file'}
+disk_attr = {'type': DISK_TYPE}
 disk = ElementTree.Element('disk', disk_attr)
 
 # Note: driver attr format is as follows:
 # name: the driver (usually QEMU)
 # driver: the subdriver (raw, qcow2, qed, etc)
-driver_attr = {'name': 'qemu', 'driver': 'qcow2'}
+driver_attr = {'name': DRIVER_NAME, 'driver': DRIVER_SUBDRIVER}
 driver = ElementTree.SubElement(disk, 'driver', driver_attr)
 
-# Source attr contains the details of the source disk, in the
-target_pool = client.storagePoolLookupByName('vm-ubuntu-focal-lxd-1')
-target_vol = target_pool.storageVolLookupByName('disk-2.img')
-
-source_attr = {'file': target_vol.path()}
+source_attr = {'file': SOURCE_DISK}
 source = ElementTree.SubElement(disk, 'source', source_attr)
 
 # Target dict contains the target device and the type of bus available
-target_attr = {'dev': 'sdd', 'bus': 'scsi'}
+target_attr = {'dev': TARGET_DEVICE, 'bus': TARGET_BUS}
 target = ElementTree.SubElement(disk, 'target', target_attr)
 
 disk_xml = ElementTree.tostring(disk, 'unicode')
 
 print(disk_xml)
-domain = client.lookupByName("vm-ubuntu-focal-lxd-1")
+domain = client.lookupByName(TARGET_DOMAIN)
 domain.attachDevice(disk_xml)
 
 
