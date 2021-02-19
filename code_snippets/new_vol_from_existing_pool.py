bfd2367e0eebcd3a846cfe97b10e58391bd3b5bb
Chong Jin Yi <chong601.13@gmail.com>
2021-02-19 13:47:39 +0800

    Add placeholder code for custom disk creation

--- code_snippets/new_vol_from_existing_pool.py
+++ code_snippets/new_vol_from_existing_pool.py
@@ -19,20 +19,29 @@ target = ElementTree.SubElement(volume, 'target')
 format_attr = {'type': DISK_TYPE}
 disk_format = ElementTree.SubElement(target, 'format', format_attr)
 
+
 # TODO: look into key
+def call_qemu_img(self, source, dest, target_disk_format, disk_capacity, allocation_method):
+    pass
+
 
 volume_xml = ElementTree.tostring(volume, 'unicode')
 print(volume_xml)
-
 client = libvirt.open("qemu+ssh://chong601@10.102.0.5/system")
 pool = client.storagePoolLookupByName(POOL_NAME)
+
 # WARNING: ZFS DO NOT support falloc method of disk allocation
 # TODO: add more disk type for other filesystems
 if DISK_TYPE == 'qcow2':
     if HOST_FILESYSTEM == 'zfs':
         # TODO: ZFS-specific metadata preallocation
+        # FUCK YOU LIBVIRT
+        # call_qemu_img(source,dest,target_disk_format,disk_capacity,'metadata')
         pass
     else:
         volume_libvirt = pool.createXML(volume_xml, libvirt.VIR_STORAGE_VOL_CREATE_PREALLOC_METADATA)
 else:
     print("Storage type is not implemented (yet)")
+
+
+
