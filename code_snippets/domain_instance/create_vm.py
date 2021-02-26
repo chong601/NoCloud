48daee6ddb5fac6bb8a34a7be6601190057c4896
Chong Jin Yi <chong601.13@gmail.com>
2021-02-26 17:58:21 +0800

    First attempt of making create VM code. And I hate it. And I'm salty. And I'm dying at the heat of Malaysian summer. Please give me cold, just for me to finish this. Thanks.

--- code_snippets/domain_instance/create_vm.py
+++ code_snippets/domain_instance/create_vm.py
@@ -13,41 +13,46 @@
 from jinja2 import FileSystemLoader, Environment, select_autoescape
 import subprocess, libvirt
 from xml.etree import ElementTree
+from uuid import UUID
+
+# String definitions
+ERR_POOL_NOT_FOUND = 'Pool "{}" is not found.'
+ERR_VOLUME_NOT_FOUND = 'Volume "{}" is not found in pool "{}".'
+ERR_DOMAIN_NOT_FOUND = 'Domain "{}" is not found.'
 
 # Parameters
+MEGABYTES = 1024**2
+GIGABYTES = 1024**3
+
 DOMAIN_NAME = 'vm-ubuntu-focal-cloud-init-test-4'
 CLOUD_DS = 'ds=nocloud-net;s=http://{}:{}/{}/'
+DISK_NAME = 'disk-2.img'
+DISK_TYPE = 'qcow2'
+DISK_CAPACITY = 100*GIGABYTES
+HOST_FILESYSTEM = 'zfs'
+POOL_NAME = 'vm-ubuntu-focal-lxd-1'
 
 # Internal definitions
 ENABLED_ESCAPES = ['html', 'xml']
+DS_HOST = '10.102.7.97'
+DS_PORT = '5000'
 j2_env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(ENABLED_ESCAPES)
 )
 
-
 client = libvirt.open("qemu+ssh://chong601@10.102.0.5/system")
-
 
 # Clone the VM pool
 subprocess.run([])
+
 # Create a new pool
 # TODO: write pool storage code
 
 # Define a new volume for libvirt to monitor
-# Constants
-MEGABYTES = 1024**2
-GIGABYTES = 1024**3
-
-# Parameters
-DISK_NAME = 'disk-2.img'
-DISK_TYPE = 'qcow2'
-HOST_FILESYSTEM = 'zfs'
-POOL_NAME = 'vm-ubuntu-focal-lxd-1'
-
 volume = ElementTree.Element('volume')
 name = ElementTree.SubElement(volume, 'name').text = DISK_NAME
-capacity = ElementTree.SubElement(volume, 'capacity').text = str(1*GIGABYTES)
+capacity = ElementTree.SubElement(volume, 'capacity').text = str(DISK_CAPACITY)
 target = ElementTree.SubElement(volume, 'target')
 # format_attr has type which is the type of disk to create eg: raw, bochs, qcow, qcow2, qed, vmdk
 format_attr = {'type': DISK_TYPE}
@@ -78,21 +83,90 @@ else:
     print("Storage type is not implemented (yet)")
 # Expand the disk to appropriate size
 # TODO: write vol-resize code
+pool = disk = None
+
+try:
+    pool = client.storagePoolLookupByName(POOL_NAME)
+except libvirt.libvirtError:
+    print(ERR_POOL_NOT_FOUND.format(POOL_NAME))
+    exit(1)
+if pool.isActive() == libvirt.VIR_STORAGE_POOL_INACTIVE:
+    pool.create()
+try:
+    disk = pool.storageVolLookupByName(DISK_NAME)
+except libvirt.libvirtError:
+    print(ERR_VOLUME_NOT_FOUND.format(DISK_NAME, POOL_NAME))
+    exit(1)
+
+# From libvirt volume resize documentation:
+#
+# > Normally, the operation treats @capacity as the new size in bytes;
+# > but if @flags contains VIR_STORAGE_VOL_RESIZE_DELTA,
+# > then @capacity represents the size difference to add to the current size.
+# > It is up to the storage pool implementation whether unaligned requests
+# > are rounded up to the next valid boundary, or rejected.
+#
+# OK libvirt, you do you I guess.
+# This is the right place to use absolute size resizing.
+# TODO: do proper size checks before blindly resizing them
+disk.resize(DISK_CAPACITY, libvirt.VIR_STORAGE_VOL_RESIZE_DELTA)
 
 # Generate domain XML
 # TODO: complete Jinja2 XML implementation
+# LAZINESS SUCK. Even if I have Jinja2 imports included LOL.`
 
 # Define in libvirt
 # TODO: write define domain code
+domain = client.defineXML()
+
 
 # Get UUID
 # TODO: write getUUID() code
+try:
+    domain_obj = client.lookupByName(DOMAIN_NAME)
+except libvirt.libvirtError:
+    print(ERR_DOMAIN_NOT_FOUND.format(DOMAIN_NAME))
+    exit(1)
+domain_uuid = domain_obj.UUID()
+# The UUID is returned in the form of actual bytes...
+# What the fuck.
+domain_uuid = domain_obj.UUID()
 
 # Set SMBIOS data
 # FIXME: can be combined into "Generate domain XML" section
+# Absolute cancer.
+# Why I need to do it this way.
+final_smbios_data = CLOUD_DS.format(DS_HOST, DS_PORT, UUID(bytes=domain_uuid))
 
 # Generate user-data
 # TODO: Create sample helper code to expose user-data
+# ALSO TODO: Move to ruamel.yaml if don't want to use `#cloud-config` line hack
+# Cue the world's possibly drunkest and most hacky way to generate YAML.
+
+#cloud-config
+# name: chong601
+# password: chong601
+# chpasswd: {expire: False}
+# ssh_pwauth: True
+# hostname: vm - ubuntu - focal - lxd - cluster - 3
+# timezone: "Asia/Kuala_Lumpur"
+# package_update: true
+# package_upgrade: true
+# package_reboot_if_required: true
+# packages:
+# - qemu - guest - agent
+# - haveged
+# power_state:
+# delay: now
+# mode: reboot
+# message: "Cloud-config for vm-ubuntu-focal-lxd-cluster-3 is completed. Restarting..."
+# timeout: 15
+# condition: True
+# system_info:
+#   default_user:
+
+
+
 
 # Generate meta-data
 # TODO: Create sample helper code to expose user-data
